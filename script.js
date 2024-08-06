9d7312b1d657ab86c9707a7adff01c2310f09671
deramsey <ramseyd@clevelandcc.edu>
2024-08-06 09:19:04 -0400

    Add options for group chat link creation

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const genLink = () =>{
+const getType = () =>{
+    if(document.querySelector("#add_one").checked){
+        genOne();
+    }
+    else if(document.querySelector("#add_group").checked){
+        genGroup();
+    }
+    else{
+        alert("Please choose a link type.")
+    }
+
+}
+
+const genOne = () =>{
     let addURL = "https://teams.microsoft.com/l/chat/0/0?users="
     let emailAdd = document.querySelector("#email_add").value;
     let URLOut = addURL + emailAdd;
     document.querySelector("#link_text").innerHTML = URLOut;
+}
+
+const genGroup = () =>{
+    let groupAdd = document.querySelector("#email_add").value;
+    let groupArr = groupAdd.split("/");
+    console.log(groupArr);
+    let URLOut = "https://teams.microsoft.com/l/chat/" + groupArr[5];
+    document.querySelector("#link_text").innerHTML = URLOut;
+    
 }
 
 const copyText = () => {
@@ -10,8 +32,8 @@ const copyText = () => {
     navigator.clipboard.writeText(textCopy);
 }
 
-document.querySelector("#submit").addEventListener("click", genLink);
+document.querySelector("#submit").addEventListener("click", getType);
 document.addEventListener("keydown", function(e){
-    if(e.key==="Enter"){ genLink() }
+    if(e.key==="Enter"){ getType() }
 });
 document.querySelector("#c").addEventListener("click", copyText);
